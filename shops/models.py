961b18ffb09c0eafd0ba6a94334ea3048c475aec
Keshav8s <97099667+Kimforee@users.noreply.github.com>
2024-10-12 23:27:34 +0530

    initial setup

---
+++ shops/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField(
+        validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)]
+    )
+    longitude = models.FloatField(
+        validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)]
+    )
+
+    def __str__(self):
+        return self.name
